f84e9b5be6090a602a2245595406bb4aa65ffc47
antoinePaulinB7 <apaulinb7@gmail.com>
2021-02-24 21:25:38 -0500

    now it makes gifs!

---
+++ antoine/2021-02-24/main.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const { createCanvas } = require('canvas');
+const GIFEncoder = require('gifencoder');
+
+const width = height = 256;
+
+const encoder = new GIFEncoder(width, height);
+// stream the results as they are available into myanimated.gif
+encoder.createReadStream().pipe(fs.createWriteStream('animated.gif'));
+ 
+encoder.start();
+encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+encoder.setDelay(200);  // frame delay in ms
+encoder.setQuality(10); 
+
+const canvas = createCanvas(width, height);
+const context = canvas.getContext('2d');
+
+Number.prototype.clamp = function(min, max) {
+  return Math.min(Math.max(this, min), max);
+};
+
+const modf = (value) => {
+  return (value > 250) ? value : 0; 
+}
+
+const rgb = (r, g, b, modf) => {
+  r = modf(Math.floor(r).clamp(0, 255));
+  g = modf(Math.floor(g).clamp(0, 255));
+  b = modf(Math.floor(b).clamp(0, 255));
+
+  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+}
+
+let startValue = 157;
+let multiplier = 1.00000089;
+
+for (let i = 0; i < 100; i ++) {
+  context.fillStyle = `#000000`;
+  context.fillRect(0, 0, width, height);
+  
+  let quotient = 1 / (startValue);
+  let offsetX = 128;
+  let offsetY = 128;
+
+  startValue *= multiplier;
+
+  let f = (x, y) => (((x - offsetX) * (x - offsetX) + (y - offsetY) * (y - offsetY)) / quotient) % 255;
+  
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      context.fillStyle = rgb(f(x, y), f(x, y), f(x, y), modf);
+      context.fillRect(x, y, 1, 1);
+    }
+  }
+
+  encoder.addFrame(context);
+}
+
+encoder.finish();
+
+// const buffer = canvas.toBuffer('image/png', {compressionLevel: 8});
+// fs.writeFileSync('out.png', buffer);
